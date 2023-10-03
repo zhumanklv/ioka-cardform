b20807a1cb132c3dd8ffd5a7527802cbfb26b722
Daniyel <daniyel.zhumankulov@nu.edu.kz>
2023-10-03 09:20:35 +0600

    create a project and configure absolute path

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import CardForm from '@/components/CardForm';
+function App() {
+  return (
+    <CardForm />
+  )
+}
+
+export default App
---
+++ src/components/CardForm.tsx
@@ -0,0 +1,12 @@
+import {FormikProvider, Formik, useFormik} from 'formik';
+
+const CardForm = () => {
+
+    return (
+   
+                <h1>hello</h1>
+
+    )
+}
+
+export default CardForm;
